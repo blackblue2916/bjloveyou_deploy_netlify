9e23be4b34d90ab618df8265f7730d10b5d6ecdf
blackblue2916 <dl2008bogu2016@gmail.com>
2022-08-10 08:00:17 +0800

    2208100800

--- src/App.js
+++ src/App.js
@@ -60,15 +60,16 @@ function App() {
   useEffect(() => {
     // GET API 免費舞蹈視頻
     fetch(
-      process.env.REACT_APP_FREE_VIDEOS_01 ||
-        process.env.REACT_APP_FREE_VIDEOS_02
+      // process.env.REACT_APP_FREE_VIDEOS_01 ||
+      process.env.REACT_APP_FREE_VIDEOS_02
     )
       .then((res) => res.json())
       .then((data) => setFreeVideosList(data));
 
     // GET API 網友提供視頻(有水印或雜項)
     fetch(
-      process.env.REACT_APP_FAN_VIDEOS_01 || process.env.REACT_APP_FAN_VIDEOS_02
+      // process.env.REACT_APP_FAN_VIDEOS_01 ||
+      process.env.REACT_APP_FAN_VIDEOS_02
     )
       .then((res) => res.json())
       .then((data) => setFanVideosList(data));
@@ -83,47 +84,40 @@ function App() {
     //   });
 
     // GET API 會員舞蹈視頻
-    axios
-      .get(
-        process.env.REACT_APP_VIP_VIDEOS_01
-        // ||
-        // process.env.REACT_APP_VIP_VIDEOS_02
-      )
-      .then((res) => {
-        setVipVideos(res.data);
-      });
+    fetch(
+      // process.env.REACT_APP_VIP_VIDEOS_01
+      // ||
+      process.env.REACT_APP_VIP_VIDEOS_02
+    )
+      .then((res) => res.json())
+      .then((data) => setVipVideos(data));
 
     // GET API 寫真視頻
-    axios
-      .get(
-        process.env.REACT_APP_IV_VIDEOS_01
-        // || process.env.REACT_APP_IV_VIDEOS_02
-      )
-      .then((res) => {
-        setIvVideos(res.data);
-      });
+    fetch(
+      // process.env.REACT_APP_IV_VIDEOS_01
+      // ||
+      process.env.REACT_APP_IV_VIDEOS_02
+    )
+      .then((res) => res.json())
+      .then((data) => setIvVideos(data));
 
     // GET API ASMR視頻
-    axios
-      .get(
-        process.env.REACT_APP_ASMR_VIDEOS_01
-        // ||
-        // process.env.REACT_APP_ASMR_VIDEOS_02
-      )
-      .then((res) => {
-        setAsmrVideos(res.data);
-      });
+    fetch(
+      // process.env.REACT_APP_ASMR_VIDEOS_01
+      // ||
+      process.env.REACT_APP_ASMR_VIDEOS_02
+    )
+      .then((res) => res.json())
+      .then((data) => setAsmrVideos(data));
 
     // GET API 音頻MP4視頻
-    axios
-      .get(
-        process.env.REACT_APP_FULIASMR_VIDEOS_01
-        // ||
-        // process.env.REACT_APP_FULIASMR_VIDEOS_02
-      )
-      .then((res) => {
-        setFuliAsmrVideos(res.data);
-      });
+    fetch(
+      // process.env.REACT_APP_FULIASMR_VIDEOS_01
+      // ||
+      process.env.REACT_APP_FULIASMR_VIDEOS_02
+    )
+      .then((res) => res.json())
+      .then((data) => setFuliAsmrVideos(data));
 
     // Git fuli videos links 福利ASMR定制視頻
 
